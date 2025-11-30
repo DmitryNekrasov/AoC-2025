b556a486acf73e97349522dec6ea687e75b63a1b
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-01 00:32:02 +0400

    Implement Day01 with parts 1 and 2, and add unit tests

---
+++ src/test/kotlin/day01/Day01Test.kt
@@ -0,0 +1,40 @@
+package day01
+
+import Day01
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import java.io.File
+
+class Day01Test {
+    private fun readInput(fileName: String): List<String> {
+        return File("src/test/kotlin/day01/$fileName").readLines()
+    }
+
+    @Test
+    fun testPart1WithTestData() {
+        val input = readInput("Day01_test01.txt")
+        val result = Day01().part1(input)
+        assertEquals(1, result)
+    }
+
+    @Test
+    fun testPart1WithFullData() {
+        val input = readInput("Day01.txt")
+        val result = Day01().part1(input)
+        assertEquals(3, result)
+    }
+
+    @Test
+    fun testPart2WithTestData() {
+        val input = readInput("Day01_test01.txt")
+        val result = Day01().part2(input)
+        assertEquals(1, result)
+    }
+
+    @Test
+    fun testPart2WithFullData() {
+        val input = readInput("Day01.txt")
+        val result = Day01().part2(input)
+        assertEquals(3, result)
+    }
+}
