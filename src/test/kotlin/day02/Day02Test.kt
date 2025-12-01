cff64794be5f9c89d1b7d1480d3ce41296125bd5
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-01 13:15:39 +0400

    Implement Day02 with parts 1 and 2, and add unit tests

---
+++ src/test/kotlin/day02/Day02Test.kt
@@ -0,0 +1,48 @@
+package day02
+
+import Day02
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class Day02Test {
+
+    companion object Companion {
+        const val DAY = "02"
+        val solution = Day02()
+    }
+
+
+    private fun readInput(fileName: String): List<String> {
+        return File("src/test/kotlin/day$DAY/$fileName")
+            .readLines()
+    }
+
+    @Test
+    fun testPart1WithTestData() {
+        val input = readInput("Day${DAY}_test01.txt")
+        val result = solution.part1(input)
+        assertEquals(1, result)
+    }
+
+    @Test
+    fun testPart1WithFullData() {
+        val input = readInput("Day${DAY}.txt")
+        val result = solution.part1(input)
+        println("result = $result")
+    }
+
+    @Test
+    fun testPart2WithTestData() {
+        val input = readInput("Day${DAY}_test01.txt")
+        val result = solution.part2(input)
+        assertEquals(10, result)
+    }
+
+    @Test
+    fun testPart2WithFullData() {
+        val input = readInput("Day${DAY}.txt")
+        val result = solution.part2(input)
+        println("result = $result")
+    }
+}
