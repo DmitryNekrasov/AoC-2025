6c7dbc53ecd23640cb7ef9d6f31b6d2df15962d8
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-02 10:42:50 +0400

    Day 2, Part 1: AC

--- src/main/kotlin/day02/Day02.kt
+++ src/main/kotlin/day02/Day02.kt
@@ -1,8 +1,50 @@
 package day02
 
 class Day02 {
+
+    fun Long.base(): Long = toString().base()
+
+    fun String.base(): Long {
+        if (length % 2 == 1) return "1${"0".repeat(length)}".base()
+        val left = take(length / 2).toLong()
+        val right = drop(length / 2).toLong()
+        return if (left < right) left + 1 else left
+    }
+
+    fun Long.multiplier(): Long {
+        var x = this
+        var result = 1L
+        while (x > 0) {
+            x /= 10
+            result *= 10L
+        }
+        return result
+    }
+
+    fun sum(from: Long, to: Long): Long = (from + to) * (to - from + 1) / 2
+
     fun part1(input: List<Pair<Long, Long>>): Long {
-        return input.size.toLong()
+        var result = 0L
+        for ((start, end) in input) {
+            val baseStart = start.base()
+            val baseEnd = end.base()
+            val baseMiddle = baseEnd - 1
+
+            val multiplierStart = baseStart.multiplier()
+            val multiplierEnd = baseEnd.multiplier()
+
+            val firstPart = sum(baseStart, baseMiddle)
+            result += firstPart * multiplierStart + firstPart
+
+            val secondPart = sum(baseMiddle + 1, baseEnd)
+            result += secondPart * multiplierEnd + secondPart
+
+            val lastNum = baseEnd * multiplierEnd + baseEnd
+            if (lastNum > end) {
+                result -= lastNum
+            }
+        }
+        return result
     }
 
     fun part2(input: List<Pair<Long, Long>>): Long {
