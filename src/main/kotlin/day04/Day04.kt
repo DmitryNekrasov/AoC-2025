5061a24f55f1a7a549b1850b21ac00871b747deb
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-03 11:12:41 +0400

    Day 4: Add initial solution with tests

---
+++ src/main/kotlin/day04/Day04.kt
@@ -0,0 +1,11 @@
+package day04
+
+class Day04 {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size * 10
+    }
+}
---
+++ src/test/kotlin/day04/Day04Test.kt
@@ -0,0 +1,47 @@
+package day04
+
+import java.io.File
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day04Test {
+
+    companion object Companion {
+        const val DAY = "04"
+        val solution = Day04()
+    }
+
+
+    private fun readInput(fileName: String): List<String> {
+        return File("src/test/kotlin/day$DAY/$fileName")
+            .readLines()
+    }
+
+    @Test
+    fun testPart1WithTestData() {
+        val input = readInput("Day${DAY}_test01.txt")
+        val result = solution.part1(input)
+        assertEquals(1, result)
+    }
+
+    @Test
+    fun testPart1WithFullData() {
+        val input = readInput("Day${DAY}.txt")
+        val result = solution.part1(input)
+        println("result = $result")
+    }
+
+    @Test
+    fun testPart2WithTestData() {
+        val input = readInput("Day${DAY}_test01.txt")
+        val result = solution.part2(input)
+        assertEquals(10, result)
+    }
+
+    @Test
+    fun testPart2WithFullData() {
+        val input = readInput("Day${DAY}.txt")
+        val result = solution.part2(input)
+        println("result = $result")
+    }
+}
