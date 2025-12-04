9e32dfdcf64fa34fe7b41babc603c632d7c6625a
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-04 11:17:34 +0400

    Day 4: Extract `countAround` helper to simplify grid processing

--- src/main/kotlin/day04/Day04.kt
+++ src/main/kotlin/day04/Day04.kt
@@ -1,6 +1,18 @@
 package day04
 
 class Day04 {
+    fun List<CharArray>.countAround(i: Int, j: Int): Int {
+        var count = 0
+        for (k in (i - 1)..(i + 1)) {
+            for (l in (j - 1)..(j + 1)) {
+                if (k in 0..<size && l in 0..<first().size && this[k][l] == '@') {
+                    count++
+                }
+            }
+        }
+        return count
+    }
+
     fun part1(grid: List<CharArray>): Int {
         val n = grid.size
         val m = grid.first().size
@@ -8,15 +20,7 @@ class Day04 {
         for (i in 0..<n) {
             for (j in 0..<m) {
                 if (grid[i][j] == '@') {
-                    var count = 0
-                    for (k in (i - 1)..(i + 1)) {
-                        for (l in (j - 1)..(j + 1)) {
-                            if (k in 0..<n && l in 0..<m && grid[k][l] == '@') {
-                                count++
-                            }
-                        }
-                    }
-                    if (count <= 4) {
+                    if (grid.countAround(i, j) <= 4) {
                         result++
                     }
                 }
@@ -35,15 +39,7 @@ class Day04 {
             for (i in 0..<n) {
                 for (j in 0..<m) {
                     if (grid[i][j] == '@') {
-                        var count = 0
-                        for (k in (i - 1)..(i + 1)) {
-                            for (l in (j - 1)..(j + 1)) {
-                                if (k in 0..<n && l in 0..<m && grid[k][l] == '@') {
-                                    count++
-                                }
-                            }
-                        }
-                        if (count <= 4) {
+                        if (grid.countAround(i, j) <= 4) {
                             grid[i][j] = '.'
                             wasRemoved = true
                             result++
