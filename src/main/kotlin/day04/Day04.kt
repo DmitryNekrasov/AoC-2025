3e20a70af1e07fbe6d4b17fad090d9feba8c87e0
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-04 11:14:37 +0400

    Day 4, Part 2: AC

--- src/main/kotlin/day04/Day04.kt
+++ src/main/kotlin/day04/Day04.kt
@@ -25,7 +25,33 @@ class Day04 {
         return result
     }
 
-    fun part2(input: List<CharArray>): Int {
-        return input.size * 10
+    fun part2(grid: List<CharArray>): Int {
+        val n = grid.size
+        val m = grid.first().size
+        var result = 0
+        var wasRemoved = true
+        while (wasRemoved) {
+            wasRemoved = false
+            for (i in 0..<n) {
+                for (j in 0..<m) {
+                    if (grid[i][j] == '@') {
+                        var count = 0
+                        for (k in (i - 1)..(i + 1)) {
+                            for (l in (j - 1)..(j + 1)) {
+                                if (k in 0..<n && l in 0..<m && grid[k][l] == '@') {
+                                    count++
+                                }
+                            }
+                        }
+                        if (count <= 4) {
+                            grid[i][j] = '.'
+                            wasRemoved = true
+                            result++
+                        }
+                    }
+                }
+            }
+        }
+        return result
     }
 }
