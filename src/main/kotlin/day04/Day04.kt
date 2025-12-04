75321c660d31c732dceb8500f9bd8b39e1b261c5
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-04 10:46:18 +0400

    Day 4, Part 1: Adjust input type to `List<CharArray>` and update related tests

--- src/main/kotlin/day04/Day04.kt
+++ src/main/kotlin/day04/Day04.kt
@@ -1,11 +1,13 @@
 package day04
 
 class Day04 {
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<CharArray>): Int {
+        println(input.size)
+
         return input.size
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<CharArray>): Int {
         return input.size * 10
     }
 }
--- src/test/kotlin/day04/Day04Test.kt
+++ src/test/kotlin/day04/Day04Test.kt
@@ -12,16 +12,16 @@ class Day04Test {
     }
 
 
-    private fun readInput(fileName: String): List<String> {
+    private fun readInput(fileName: String): List<CharArray> {
         return File("src/test/kotlin/day$DAY/$fileName")
-            .readLines()
+            .readLines().map { it.toCharArray() }
     }
 
     @Test
     fun testPart1WithTestData() {
         val input = readInput("Day${DAY}_test01.txt")
         val result = solution.part1(input)
-        assertEquals(1, result)
+        assertEquals(13, result)
     }
 
     @Test
