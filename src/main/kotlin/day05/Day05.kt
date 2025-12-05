b6187febd79e2947bbcfc1c9acf2fb011b483b67
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-05 10:05:32 +0400

    Day 5, Part 1: AC

--- src/main/kotlin/day05/Day05.kt
+++ src/main/kotlin/day05/Day05.kt
@@ -1,9 +1,8 @@
 package day05
 
 class Day05 {
-    fun part1(ranges: List<LongRange>, nums: List<Long>): Int {
-        return ranges.size
-    }
+    fun part1(ranges: List<LongRange>, nums: List<Long>): Int =
+        nums.count { num -> ranges.any { range -> num in range } }
 
     fun part2(ranges: List<LongRange>, nums: List<Long>): Int {
         return ranges.size * 10
