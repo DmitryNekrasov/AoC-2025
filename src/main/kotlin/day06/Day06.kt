890cae410c47191f3b7e6dbf63e7e434a346341a
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-06 11:54:09 +0400

    Day 6, Part 1: AC

--- src/main/kotlin/day06/Day06.kt
+++ src/main/kotlin/day06/Day06.kt
@@ -2,9 +2,18 @@ package day06
 
 class Day06 {
     fun part1(nums: List<List<Long>>, operations: List<Char>): Long {
-        nums.joinToString("\n").also { println(it) }
-        println(operations)
-        return 0L
+        var result = 0L
+        for (i in operations.indices) {
+            var current = if (operations[i] == '+') 0L else 1L
+            for (numList in nums) {
+                when (operations[i]) {
+                    '*' -> current *= numList[i]
+                    else -> current += numList[i]
+                }
+            }
+            result += current
+        }
+        return result
     }
 
     fun part2(nums: List<List<Long>>, operations: List<Char>): Long {
