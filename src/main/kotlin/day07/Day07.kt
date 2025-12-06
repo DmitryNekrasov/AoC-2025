42ae0188abe44b5b2f1e726081f0b96706c839ed
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-06 17:54:46 +0400

    Day 7: Add initial solution with tests

---
+++ src/main/kotlin/day07/Day07.kt
@@ -0,0 +1,11 @@
+package day07
+
+class Day07 {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size * 10
+    }
+}
