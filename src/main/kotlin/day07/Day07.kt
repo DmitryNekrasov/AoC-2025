4950f064dcba2c84e220738fc800dda915303252
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-07 14:55:59 +0400

    Day 7, Part 1: AC

--- src/main/kotlin/day07/Day07.kt
+++ src/main/kotlin/day07/Day07.kt
@@ -2,7 +2,23 @@ package day07
 
 class Day07 {
     fun part1(grid: List<CharArray>): Int {
-        return 0
+        val n = grid.size
+        val m = grid.first().size
+        var result = 0
+        for (i in 1..<n) {
+            for (j in 0..<m) {
+                if (grid[i - 1][j] == 'S' || grid[i - 1][j] == '|') {
+                    if (grid[i][j] == '.') {
+                        grid[i][j] = '|'
+                    } else if (grid[i][j] == '^') {
+                        grid[i][j - 1] = '|'
+                        grid[i][j + 1] = '|'
+                        result++
+                    }
+                }
+            }
+        }
+        return result
     }
 
     fun part2(input: List<CharArray>): Int {
