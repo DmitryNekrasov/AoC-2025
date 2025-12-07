6a1ce142770b5affba372ef31000632436b4e788
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-07 15:28:13 +0400

    Day 8: Add initial solution with tests

---
+++ src/test/kotlin/day08/Day08Test.kt
@@ -0,0 +1,46 @@
+package day08
+
+import java.io.File
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day08Test {
+
+    companion object Companion {
+        const val DAY = "08"
+        val solution = Day08()
+    }
+
+    private fun readInput(fileName: String): List<String> {
+        return File("src/test/kotlin/day$DAY/$fileName")
+            .readLines()
+    }
+
+    @Test
+    fun testPart1WithTestData() {
+        val input = readInput("Day${DAY}_test01.txt")
+        val result = solution.part1(input)
+        assertEquals(1, result)
+    }
+
+    @Test
+    fun testPart1WithFullData() {
+        val input = readInput("Day${DAY}.txt")
+        val result = solution.part1(input)
+        println("result = $result")
+    }
+
+    @Test
+    fun testPart2WithTestData() {
+        val input = readInput("Day${DAY}_test01.txt")
+        val result = solution.part2(input)
+        assertEquals(10, result)
+    }
+
+    @Test
+    fun testPart2WithFullData() {
+        val input = readInput("Day${DAY}.txt")
+        val result = solution.part2(input)
+        println("result = $result")
+    }
+}
