7203685ae9e20a5dfc344356d72ba7baf7650bd6
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-07 15:18:20 +0400

    Day 7, Part 2: AC

--- src/main/kotlin/day07/Day07.kt
+++ src/main/kotlin/day07/Day07.kt
@@ -21,7 +21,26 @@ class Day07 {
         return result
     }
 
-    fun part2(input: List<CharArray>): Long {
-        return 1L
+    fun part2(grid: List<CharArray>): Long {
+        val n = grid.size
+        val m = grid.first().size
+        val dp = Array(n) { LongArray(m) }
+        dp[0][grid.first().indexOf('S')] = 1L
+        for (i in 1..<n) {
+            for (j in 0..<m) {
+                if (grid[i - 1][j] == 'S' || grid[i - 1][j] == '|') {
+                    if (grid[i][j] == '.' || grid[i][j] == '|') {
+                        grid[i][j] = '|'
+                        dp[i][j] += dp[i - 1][j]
+                    } else if (grid[i][j] == '^') {
+                        grid[i][j - 1] = '|'
+                        grid[i][j + 1] = '|'
+                        dp[i][j - 1] += dp[i - 1][j]
+                        dp[i][j + 1] += dp[i - 1][j]
+                    }
+                }
+            }
+        }
+        return dp[n - 1].sum()
     }
 }
