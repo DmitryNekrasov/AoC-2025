3351b89dfd059cac2ad2c524b24321ad13a9416d
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-07 14:49:24 +0400

    Day 7: Update solution and tests to use grid-based input instead of string-based input

--- src/main/kotlin/day07/Day07.kt
+++ src/main/kotlin/day07/Day07.kt
@@ -1,11 +1,11 @@
 package day07
 
 class Day07 {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(grid: List<CharArray>): Int {
+        return 0
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size * 10
+    fun part2(input: List<CharArray>): Int {
+        return 1
     }
 }
--- src/test/kotlin/day07/Day07Test.kt
+++ src/test/kotlin/day07/Day07Test.kt
@@ -11,36 +11,36 @@ class Day07Test {
         val solution = Day07()
     }
 
-    private fun readInput(fileName: String): List<String> {
+    private fun readInput(fileName: String): List<CharArray> {
         return File("src/test/kotlin/day$DAY/$fileName")
-            .readLines()
+            .readLines().map(String::toCharArray)
     }
 
     @Test
     fun testPart1WithTestData() {
-        val input = readInput("Day${DAY}_test01.txt")
-        val result = solution.part1(input)
-        assertEquals(1, result)
+        val grid = readInput("Day${DAY}_test01.txt")
+        val result = solution.part1(grid)
+        assertEquals(21, result)
     }
 
     @Test
     fun testPart1WithFullData() {
-        val input = readInput("Day${DAY}.txt")
-        val result = solution.part1(input)
+        val grid = readInput("Day${DAY}.txt")
+        val result = solution.part1(grid)
         println("result = $result")
     }
 
     @Test
     fun testPart2WithTestData() {
-        val input = readInput("Day${DAY}_test01.txt")
-        val result = solution.part2(input)
+        val grid = readInput("Day${DAY}_test01.txt")
+        val result = solution.part2(grid)
         assertEquals(10, result)
     }
 
     @Test
     fun testPart2WithFullData() {
-        val input = readInput("Day${DAY}.txt")
-        val result = solution.part2(input)
+        val grid = readInput("Day${DAY}.txt")
+        val result = solution.part2(grid)
         println("result = $result")
     }
 }
