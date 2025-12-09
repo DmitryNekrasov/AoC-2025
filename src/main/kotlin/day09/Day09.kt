638d10cbb07fe0b58e6b472e7d728170ae2c0531
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-09 10:45:36 +0400

    Day 9, Part 2: Add `toSegments` helper for converting points to segments

--- src/main/kotlin/day09/Day09.kt
+++ src/main/kotlin/day09/Day09.kt
@@ -18,7 +18,32 @@ class Day09 {
         return result
     }
 
+    data class Point(val x: Long, val y: Long) {
+        override fun toString(): String = "($x,$y)"
+    }
+
+    data class Segment(val p1: Point, val p2: Point) {
+        override fun toString(): String = "[$p1;$p2]"
+    }
+
+    fun List<Pair<Long, Long>>.toSegments(): List<Segment> {
+        val (x1, y1) = this[0]
+        val (x2, y2) = this[1]
+        val segments = mutableListOf(Segment(Point(x1, y1), Point(x2, y2)))
+        val n = size
+        for (i in 2..<n) {
+            val (x, y) = this[i]
+            segments += Segment(segments.last().p2, Point(x, y))
+        }
+        segments += Segment(segments.last().p2, segments.first().p1)
+        return segments
+    }
+
     fun part2(points: List<Pair<Long, Long>>): Long {
+        val segments = points.toSegments()
+
+        println(segments)
+
         return 1L
     }
 }
