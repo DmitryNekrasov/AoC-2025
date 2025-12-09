e8270f55affefd2beefb2b884d5b37f8fb8d74d8
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-09 14:10:33 +0400

    Day 10: Add initial setup with solution skeleton and basic tests

---
+++ src/main/kotlin/day10/Day10.kt
@@ -0,0 +1,11 @@
+package day10
+
+class Day10 {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size * 10
+    }
+}
