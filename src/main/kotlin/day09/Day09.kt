d3e958a75924e18f6018b93cc975c88a9dc77b0d
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-09 13:57:51 +0400

    Day 9, Part 1: Simplify loop bounds by replacing `n` with `points.lastIndex`

--- src/main/kotlin/day09/Day09.kt
+++ src/main/kotlin/day09/Day09.kt
@@ -5,11 +5,10 @@ import kotlin.random.Random
 
 class Day09 {
     fun part1(points: List<Pair<Long, Long>>): Long {
-        val n = points.size
         var result = 0L
-        for (i in 0..<(n - 1)) {
+        for (i in 0..<(points.lastIndex - 1)) {
             val (x1, y1) = points[i]
-            for (j in (i + 1)..<n) {
+            for (j in (i + 1)..<points.lastIndex) {
                 val (x2, y2) = points[j]
                 val width = (x1 - x2).absoluteValue + 1
                 val height = (y1 - y2).absoluteValue + 1
