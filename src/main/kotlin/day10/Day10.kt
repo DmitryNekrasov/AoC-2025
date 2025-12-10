fff75f9c6fd74fd849f2b240d3f828158278f41e
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-10 10:24:39 +0400

    Day 10, Part 1: Refactor input parsing logic and update test expectations

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -1,11 +1,15 @@
 package day10
 
+data class Data(val lightDiagram: String, val buttons: List<List<Int>>, val joltage: List<Int>)
+
 class Day10 {
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<Data>): Int {
+        input.joinToString("\n").also(::println)
+
         return input.size
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<Data>): Int {
         return input.size * 10
     }
 }
--- src/test/kotlin/day10/Day10Test.kt
+++ src/test/kotlin/day10/Day10Test.kt
@@ -7,20 +7,37 @@ import kotlin.test.assertEquals
 class Day10Test {
 
     companion object Companion {
-        const val DAY = "00"
+        const val DAY = "10"
         val solution = Day10()
     }
 
-    private fun readInput(fileName: String): List<String> {
+    private fun String.toLightDiagram(): String = substring(1, lastIndex)
+
+    private fun List<String>.toButtons(): List<List<Int>> =
+        map { it.substring(1, it.lastIndex).split(",").map(String::toInt) }
+
+    private fun String.toJoltage(): List<Int> =
+        substring(1, lastIndex).split(",").map(String::toInt)
+
+    private fun readInput(fileName: String): List<Data> {
         return File("src/test/kotlin/day$DAY/$fileName")
             .readLines()
+            .map { line ->
+                line.split(" ").let { tokens ->
+                    Data(
+                        lightDiagram = tokens.first().toLightDiagram(),
+                        buttons = tokens.subList(1, tokens.lastIndex).toButtons(),
+                        joltage = tokens.last().toJoltage()
+                    )
+                }
+            }
     }
 
     @Test
     fun testPart1WithTestData() {
         val input = readInput("Day${DAY}_test01.txt")
         val result = solution.part1(input)
-        assertEquals(1, result)
+        assertEquals(7, result)
     }
 
     @Test
