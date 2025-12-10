a023bec9336ed28818905da2d79212aa054ac963
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-10 11:44:10 +0400

    Day 10, Part 1: Inline helper functions for `compress` and `bfs` into class for improved encapsulation

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -3,41 +3,41 @@ package day10
 import java.util.LinkedList
 import java.util.Queue
 
-private fun String.compress(): Int {
-    var result = 0
-    for ((i, c) in this.withIndex()) {
-        if (c == '#') {
-            result = result or (1 shl i)
-        }
-    }
-    return result
-}
-
-private fun List<List<Int>>.compress(): List<Int> = map { it.fold(0) { acc, num -> acc or (1 shl num) } }
-
 data class Data(val lightDiagram: String, val buttons: List<List<Int>>, val joltage: List<Int>)
-
-fun bfs(target: Int, transitions: List<Int>): Int {
-    val start = 0
-    val queue: Queue<Pair<Int, Int>> = LinkedList()
-    queue.add(start to 0)
-    val visited = hashSetOf(start)
-    while (queue.isNotEmpty()) {
-        val (from, distance) = queue.poll()
-        if (from == target) return distance
-        for (transition in transitions) {
-            val to = from xor transition
-            if (to !in visited) {
-                visited.add(to)
-                queue.add(to to distance + 1)
-            }
-        }
-    }
-    error("Should not reach here")
-}
 
 class Day10 {
     fun part1(input: List<Data>): Int {
+        fun String.compress(): Int {
+            var result = 0
+            for ((i, c) in this.withIndex()) {
+                if (c == '#') {
+                    result = result or (1 shl i)
+                }
+            }
+            return result
+        }
+
+        fun List<List<Int>>.compress(): List<Int> = map { it.fold(0) { acc, num -> acc or (1 shl num) } }
+
+        fun bfs(target: Int, transitions: List<Int>): Int {
+            val start = 0
+            val queue: Queue<Pair<Int, Int>> = LinkedList()
+            queue.add(start to 0)
+            val visited = hashSetOf(start)
+            while (queue.isNotEmpty()) {
+                val (from, distance) = queue.poll()
+                if (from == target) return distance
+                for (transition in transitions) {
+                    val to = from xor transition
+                    if (to !in visited) {
+                        visited.add(to)
+                        queue.add(to to distance + 1)
+                    }
+                }
+            }
+            error("Should not reach here")
+        }
+
         return input.sumOf { (lightDiagram, buttons, _) -> bfs(lightDiagram.compress(), buttons.compress()) }
     }
 
