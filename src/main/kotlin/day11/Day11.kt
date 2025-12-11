27e5c538efe4b350b98a7ab4343a699be9f70209
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-11 11:19:44 +0400

    Day 11, Parts 1 and 2: Refactor input type to `List<List<Int>>`, adjust method signatures, and update tests

--- src/main/kotlin/day11/Day11.kt
+++ src/main/kotlin/day11/Day11.kt
@@ -1,27 +1,9 @@
 package day11
 
 class Day11 {
-    class Enumerator<T> {
-        private val cache = mutableMapOf<T, Int>()
-        operator fun get(x: T): Int = cache.getOrPut(x) { cache.size }
-    }
-
-    fun part1(input: List<Pair<String, List<String>>>): Int {
-        val n = input.size + 1
-        val enumerator = Enumerator<String>()
-        val graph = Array(n) { mutableListOf<Int>() }
-        for ((fromStr, list) in input) {
-            val from = enumerator[fromStr]
-            for (toStr in list) {
-                graph[from].add(enumerator[toStr])
-            }
-        }
-
-        val start = enumerator["you"]
-        val finish = enumerator["out"]
-
+    fun part1(graph: List<List<Int>>, you: Int, out: Int): Int {
         fun dfs(from: Int): Int {
-            if (from == finish) return 1
+            if (from == out) return 1
             var count = 0
             for (to in graph[from]) {
                 count += dfs(to)
@@ -29,10 +11,10 @@ class Day11 {
             return count
         }
 
-        return dfs(start)
+        return dfs(you)
     }
 
-    fun part2(input: List<Pair<String, List<String>>>): Int {
-        return input.size * 10
+    fun part2(graph: List<List<Int>>, you: Int, out: Int, dac: Int, fft: Int): Int {
+        return 0
     }
 }
--- src/test/kotlin/day11/Day11Test.kt
+++ src/test/kotlin/day11/Day11Test.kt
@@ -11,38 +11,50 @@ class Day11Test {
         val solution = Day11()
     }
 
-    private fun readInput(fileName: String): List<Pair<String, List<String>>> {
-        return File("src/test/kotlin/day$DAY/$fileName")
-            .readLines().map { line ->
-                line.split(": ").let { (key, value) -> key to value.split(" ") }
+    class Enumerator<T> {
+        private val cache = mutableMapOf<T, Int>()
+        operator fun get(x: T): Int = cache.getOrPut(x) { cache.size }
+    }
+
+    private fun readInput(fileName: String): Pair<List<List<Int>>, Enumerator<String>> {
+        val input = File("src/test/kotlin/day$DAY/$fileName")
+            .readLines().map { line -> line.split(": ").let { (key, value) -> key to value.split(" ") } }
+        val enumerator = Enumerator<String>()
+        val graph = List(input.size + 1) { mutableListOf<Int>() }
+        for ((fromStr, list) in input) {
+            val from = enumerator[fromStr]
+            for (toStr in list) {
+                graph[from].add(enumerator[toStr])
             }
+        }
+        return graph to enumerator
     }
 
     @Test
     fun testPart1WithTestData() {
-        val input = readInput("Day${DAY}_test01.txt")
-        val result = solution.part1(input)
+        val (graph, enumerator) = readInput("Day${DAY}_test01.txt")
+        val result = solution.part1(graph, enumerator["you"], enumerator["out"])
         assertEquals(5, result)
     }
 
     @Test
     fun testPart1WithFullData() {
-        val input = readInput("Day${DAY}.txt")
-        val result = solution.part1(input)
+        val (graph, enumerator) = readInput("Day${DAY}.txt")
+        val result = solution.part1(graph, enumerator["you"], enumerator["out"])
         println("result = $result")
     }
 
     @Test
     fun testPart2WithTestData() {
-        val input = readInput("Day${DAY}_test01.txt")
-        val result = solution.part2(input)
-        assertEquals(10, result)
+        val (graph, enumerator) = readInput("Day${DAY}_test02.txt")
+        val result = solution.part2(graph, enumerator["you"], enumerator["out"], enumerator["dac"], enumerator["fft"])
+        assertEquals(2, result)
     }
 
     @Test
     fun testPart2WithFullData() {
-        val input = readInput("Day${DAY}.txt")
-        val result = solution.part2(input)
+        val (graph, enumerator) = readInput("Day${DAY}.txt")
+        val result = solution.part2(graph, enumerator["you"], enumerator["out"], enumerator["dac"], enumerator["fft"])
         println("result = $result")
     }
 }
