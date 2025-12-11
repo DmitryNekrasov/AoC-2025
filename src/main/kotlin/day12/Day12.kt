9903fd0f0011420160a2cb8f8a49ecf1a0279404
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-11 13:37:17 +0400

    Day 12: Add initial implementation for Parts 1 and 2 with unit tests

---
+++ src/main/kotlin/day12/Day12.kt
@@ -0,0 +1,11 @@
+package day12
+
+class Day12 {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size * 10
+    }
+}
