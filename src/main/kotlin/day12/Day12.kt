e0269e0e17ad9ef4b585e1cb179cedcf37394fd4
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-12-12 10:36:41 +0400

    Day 12: Add `rotate` method to `Shape` class for 90-degree clockwise rotation

--- src/main/kotlin/day12/Day12.kt
+++ src/main/kotlin/day12/Day12.kt
@@ -4,6 +4,16 @@ data class Shape(val grid: List<CharArray>) {
     override fun toString(): String = grid.joinToString("\n") { String(it) }
 }
 
+fun Shape.rotate(): Shape {
+    val rotatedGrid = List(grid.first().size) { CharArray(grid.size) { '.' } }
+    for (i in 0..<grid.size) {
+        for (j in 0..<grid.first().size) {
+            rotatedGrid[j][grid.size - i - 1] = grid[i][j]
+        }
+    }
+    return Shape(rotatedGrid)
+}
+
 data class Region(val n: Int, val m: Int, val quantities: List<Int>)
 
 class Day12 {
